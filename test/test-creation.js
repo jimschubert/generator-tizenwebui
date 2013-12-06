bba2b05e687dd578ecfebdd5513bec8fd2d47a1b
Jim Schubert <james.schubert@gmail.com>
2013-12-05 22:24:13 -0800

    Modified generated test to report correct file output

--- test/test-creation.js
+++ test/test-creation.js
@@ -21,13 +21,35 @@ describe('tizenwebui generator', function () {
 
     it('creates expected files', function (done) {
         var expected = [
-            // add files you expect to exist here.
+            'config.xml',
+            'package.json',
+            'bower.json',
+            'Gruntfile.js',
+            'src/css/main.css',
+            'src/css/normalize.css',
+            'src/css/normalize.min.css',
+            'src/js/main.js',
+            'src/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js',
+            'src/img/tizen_32.png',
+            'tools/grunt-package.js',
+            'tools/grunt-package.license',
+            'tools/scripts/launch_emulator.sh',
+            'src/icon.png',
+            'src/index.html',
+            'src/css/main.css',
+            '.editorconfig',
             '.jshintrc',
-            '.editorconfig'
+            '.gitignore',
+            '.bowerrc'
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            'projectName': 'Some example app',
+            'authorName': 'Jim Schubert',
+            'authorEmail': 'james.schubert@gmail.com',
+            'authorUrl': 'http://ipreferjim.com',
+            'tizenSdk': '~/tizen-sdk',
+            'useTizenIndex': true
         });
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
